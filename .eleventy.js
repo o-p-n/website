4405a3e3f6405d56b167fe46742e04042f8cddcb
Matthew A. Miller <linuxwolf@outer-planes.net>
2022-09-22 18:04:30 -0600

    move sources to their own sub-directory

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 module.exports = function(config) {
   config.ignores.add("README.md");
 
-  config.addPassthroughCopy(".well-known");
-  config.addPassthroughCopy("assets");
+  config.addPassthroughCopy("src/.well-known");
+  config.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: "src",
+    }
+  };
 };
