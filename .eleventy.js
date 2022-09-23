e8f6bba55644ee0c65ebe2c1e9c1bd3e4787a334
Matthew A. Miller <linuxwolf@outer-planes.net>
2022-09-23 16:14:40 -0600

    flesh out markdown support

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,26 @@
+const markdownit = {
+  _lib: require("markdown-it"),
+  anchor: require("markdown-it-anchor"),
+  attrs: require("markdown-it-attrs"),
+  container: require("markdown-it-container"),
+  footnote: require("markdown-it-footnote"),
+  toc: require("markdown-it-table-of-contents"),
+};
+
 module.exports = function(config) {
   config.ignores.add("README.md");
 
   config.addPassthroughCopy("src/.well-known");
   config.addPassthroughCopy("src/assets");
+
+  const mdLib = markdownit._lib({
+    html: true,
+  }).use(markdownit.anchor.default)
+    .use(markdownit.attrs)
+    .use(markdownit.footnote)
+    .use(markdownit.toc, { includeLevel: [2,3,4]})
+    .use(markdownit.container, "callout")
+  config.setLibrary("md", mdLib);
 
   return {
     dir: {
--- src/index.md
+++ src/index.md
@@ -1,8 +1,3 @@
----
-layout: layouts/base
-title: O U T E R   P L A N E S
----
-
-# O U T E R &nbsp; P L A N E S
+# O U T E R - P L A N E S
 
 Welcome weary traveler!
