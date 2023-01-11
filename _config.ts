a4b71f67442bdfb4928a81a51146570257c6bf30
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-01-11 08:37:38 -0700

    switch to Deno + Lume

---
+++ _config.ts
@@ -0,0 +1,30 @@
+import lume from "lume/mod.ts";
+import date from "lume/plugins/date.ts";
+import relations from "lume/plugins/relations.ts";
+import sass from "lume/plugins/sass.ts";
+
+import mdContainer from "markdown-it-container";
+import mdFootnote from "markdown-it-footnote";
+
+const markdown = {
+  plugins: [
+    [mdContainer, "callout"],
+    mdFootnote,
+  ],
+  keepDefaultPlugins: true,
+};
+
+const site = lume({
+  src: "src",
+}, { markdown });
+
+site.use(date());
+site.use(relations());
+site.use(sass({
+  format: "expanded",
+}));
+
+site.copy("assets");
+site.copy(".well-known");
+
+export default site;
