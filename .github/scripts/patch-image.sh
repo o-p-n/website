e4232e407ea7a893ff1aee5791d9b264616b86f2
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-03-08 16:31:40 -0700

    fix: workflows are failing (#23)

--- .github/scripts/patch-image.sh
+++ .github/scripts/patch-image.sh
@@ -1,11 +1,45 @@
 #! /usr/bin/env bash
 
-git config user.name "${GIT_USER_NAME}"
-git config user.email "${GIT_USER_EMAIL}"
-git checkout -b "deploy-${GIT_SHA}"
-git add --all
-git commit -am "chore: deploy ${GIT_SHA}"
-git push -u origin "deploy-${GIT_SHA}"
-gh pr create --title="chore: deploy ${GIT_SHA}" --body=""
-sleep 5
-gh pr merge --auto --squash
+set -euo pipefail
+
+loggit() {
+  echo "$@" >> /dev/stderr
+}
+
+check_clean() {
+  git update-index --refresh --ignore-submodules 2>&1 >> /dev/null
+  git diff --quiet --ignore-submodules
+}
+
+config_git() {
+  git config user.name "${GIT_USER_NAME}"
+  git config user.email "${GIT_USER_EMAIL}"
+  git checkout -b "deploy-${GIT_SHA}"
+}
+
+create_pr() {
+  git add --all
+  git commit -am "chore: deploy ${GIT_SHA}"
+  git push -u origin "deploy-${GIT_SHA}"
+  gh pr create --title="chore: deploy ${GIT_SHA}" --body=""
+}
+
+approve_pr() {
+  gh pr merge --auto --squash
+}
+
+if check_clean ; then
+  loggit "✨ — working copy clean; no changes detected"
+else
+  loggit "changes detected; update repo"
+  loggit "📝 — configure git"
+  config_git
+  loggit "💾 — creating pull-request"
+  create_pr
+  
+  loggit ⏳ — give GH some time ...
+  sleep 10
+
+  loggit "✅ — auto-approve pull-request"
+  approve_pr
+fi
