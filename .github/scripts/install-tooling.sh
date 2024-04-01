abeea7c47f06d68a5aad377cb15d649e03d338d0
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-04-01 16:06:23 -0600

    install deno et al

--- .github/scripts/install-tooling.sh
+++ .github/scripts/install-tooling.sh
@@ -7,6 +7,8 @@ loggit() {
 }
 
 INSTALL_PATH="${INSTALL_PATH:-$HOME/bin}"
+
+DENO_VERSION="${DENO_VERSION:-1.42.0}"
 DEPLOYER_VERSION="${DEPLOYER_VERSION:-0.4.1}"
 TASKFILE_VERSION="${TASKFILE_VERSION:-3.35.1}"
 CRANE_VERSION="${CRANE_VERSION:-0.19.1}"
@@ -15,6 +17,52 @@ KUBECTL_VERSION="${KUBECTL_VERSION:-1.28.3}"
 # Platform variables
 PLATFORM_ARCH=$(uname -m)
 PLATFORM_OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+function install_deno() {
+  local version="${DENO_VERSION}"
+  local platform_os="${PLATFORM_OS}"
+  local platform_arch="${PLATFORM_ARCH}"
+
+  if [[ "${version}" == "latest" ]] ; then
+    version=""
+  else
+    version="v${version}"
+  fi
+
+  case "${platform_os}" in
+    "darwin")
+      platform_os="apple-darwin"
+      ;;
+    "linux")
+      platform_os="unknown-linux-gnu"
+      ;;
+  esac
+
+  case "${platform_arch}" in
+    "amd64")
+      platform_arch="x86_64"
+      ;;
+    "arm64")
+      platform_arch="aarch64"
+      ;;
+  esac
+  platform="${platform_arch}-${platform_os}"
+
+  loggit "downloading deno ${version:-latest} for ${platform_os}/${platform_arch} ..."
+
+  mkdir -p deno && pushd deno
+
+  # TODO verify checksum
+  gh --repo denoland/deno release download \
+      --pattern "deno-${platform}.zip" --clobber \
+      --output deno.zip ${version}
+  unzip deno.zip
+
+  loggit "... installing ..."
+  install deno "${INSTALL_PATH}"
+
+  popd
+}
 
 function install_deployer() {
   local version="${DEPLOYER_VERSION}"
@@ -48,14 +96,18 @@ function install_deployer() {
 
   loggit "downloading o-p-n.deployer ${version:-latest} for ${platform_os}/${platform_arch} ..."
 
+  mkdir -p o-p-n.deployer && pushd o-p-n.deployer
+
   # TODO verify checksum
   gh --repo o-p-n/deployer release download \
       --pattern "*-${platform}.tar.gz" --clobber \
-      --output - ${version} \
-    | tar xzf -
+      --output o-p-n.deployer.tar.gz ${version}
+  tar xzf o-p-n.deployer.tar.gz
 
   loggit "... installing ..."
   install o-p-n.deployer "${INSTALL_PATH}"
+
+  popd
 }
 
 function install_taskfile() {
@@ -80,10 +132,12 @@ function install_taskfile() {
 
   loggit "downloading task ${version:-latest} for ${platform_os}/${platform_arch} ..."
 
+  mkdir -p task && pushd task
+
   gh --repo go-task/task release download \
       --pattern "task_${platform_os}_${platform_arch}.tar.gz" --clobber \
-      --output - ${version} \
-    | tar xzf - task
+      --output task.tar.gz ${version}
+  tar xzf task.tar.gz
 
   loggit "... installing ..."
   install task "${INSTALL_PATH}"
@@ -117,8 +171,8 @@ function install_crane() {
   # TODO verify checksum
   gh --repo google/go-containerregistry release download \
       --pattern "go-containerregistry_${platform_os}_${platform_arch}.tar.gz" --clobber \
-      --output - ${version} \
-    | tar xzf - crane
+      --output crane.tar.gz ${version}
+  tar xzf crane.tar.gz
 
   loggit "... installing ..."
   install crane "${INSTALL_PATH}"
@@ -146,9 +200,16 @@ function install_kubectl() {
   install kubectl "${INSTALL_PATH}"
 }
 
+working_dir=$(mktemp -d)
+pushd ${working_dir}
+
+install_deno
 install_deployer
 install_taskfile
 install_crane
 install_kubectl
+
+popd
+rm -rf ${working_dir}
 
 loggit "... DONE"
