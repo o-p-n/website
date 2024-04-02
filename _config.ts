17836ed6d6ebebc5f87f95a87251f7af739d9ad9
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-04-02 09:13:16 -0600

    use git latest-commit time

--- _config.ts
+++ _config.ts
@@ -7,6 +7,8 @@ import nunjucks from "lume/plugins/nunjucks.ts";
 import mdContainer from "markdown-it-container";
 import mdFootnote from "markdown-it-footnote";
 
+import $ from "dax";
+
 const markdown = {
   plugins: [
     [mdContainer, "callout"],
@@ -29,8 +31,10 @@ site.preprocess([".md"], async (pages) => {
   for (const p of pages) {
     const path = p.src.entry?.src || "";
     if (!path) { continue }
-    const stat = await Deno.stat(path);
-    console.log(`${p.src.path} times: created=${stat.birthtime?.toISOString()}, modified=${stat.mtime?.toISOString()}`);
+    const gitUnix = await $`git log --pretty=%ct -n 1 -- src${p.src.path}${p.src.ext}`.text();
+    const git = new Date(parseInt(gitUnix) * 1000);
+    p.data.date = git;
+    console.log(`date for ${p.src.path} is ${p.data.date.toISOString()}`);
   }
 });
 
