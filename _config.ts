7fcd6219b0178032f3d236653c873e6f7cf368b4
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-04-03 17:55:12 -0600

    chore: simplify generation (#61)
    with reproducibility seemingly handled, simplify the site generation to
    just what is required

--- _config.ts
+++ _config.ts
@@ -1,13 +1,10 @@
 import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
-import relations from "lume/plugins/relations.ts";
 import sass from "lume/plugins/sass.ts";
 import nunjucks from "lume/plugins/nunjucks.ts";
 
 import mdContainer from "markdown-it-container";
 import mdFootnote from "markdown-it-footnote";
-
-import $ from "dax";
 
 const markdown = {
   plugins: [
@@ -22,21 +19,10 @@ const site = lume({
 }, { markdown });
 
 site.use(date());
-site.use(relations());
 site.use(sass({
   format: "expanded",
 }));
 site.use(nunjucks());
-site.preprocess([".md"], async (pages) => {
-  for (const p of pages) {
-    const path = p.src.entry?.src || "";
-    if (!path) { continue }
-    const gitUnix = await $`git log --pretty=%ct -n 1 -- src${p.src.path}${p.src.ext}`.text();
-    const git = new Date(parseInt(gitUnix) * 1000);
-    p.data.date = git;
-    console.log(`date for ${p.src.path} is ${p.data.date.toISOString()}`);
-  }
-});
 
 site.copyRemainingFiles();
 site.copy("assets");
