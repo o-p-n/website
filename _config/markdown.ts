25ff90bb1d3b29121a6311a7774c6ff9ce04c0e1
Matthew A. Miller <linuxwolf@outer-planes.net>
2025-10-02 08:03:54 -0600

    feat: upgrade major dependencies (#94)

--- _config/markdown.ts
+++ _config/markdown.ts
@@ -1,9 +1,20 @@
-import container from "npm:markdown-it-container@4.0.0";
-import footnote from "npm:markdown-it-footnote@4.0.0";
+import footnote from "markdown-it-footnote";
+import callouts from "markdown-it-callouts";
 
 const markdown = {
   plugins: [
-    [container, "callout"],
+    [callouts, {
+      defaultElementType: "blockquote",
+      calloutTitleElementType: "h6",
+      emptyTitleFallback: "match-type",
+      calloutSymbols: {
+        "note": "\ue34c",
+        "tip": "\ue2dc",
+        "important": "\ue9b8",
+        "warning": "\ue4e0",
+        "caution": "\ue4e4",
+      },
+    }],
     footnote,
   ],
 };
